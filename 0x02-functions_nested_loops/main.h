441e93a1a409db391a7dc7109a8d6c478cf0f728
AbelWondimuL <abelwondimu208@gmail.com>
2022-04-12 16:08:29 -0700

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
+#ifndef _main_h_
+#define _main_h_
 
 int _putchar(char c);
 void print_alphabet(void);
@@ -13,4 +13,6 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-#endif /* _MAIN_H_ */
+void print_times_table(int n);
+
+#endif
