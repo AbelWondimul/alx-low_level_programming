1bfa44083f8ce779cf74221fb694743ab092ab62
AbelWondimuL <abelwondimu208@gmail.com>
2022-04-12 16:44:39 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
+
 /**
+ * print_alphabet_x10 - Check holberton
  * Description: function uses _putchar function to print
  * alphabet in lowercase 10 times
  * Return: Nothing.
  */
+
 void print_alphabet_x10(void)
 {
 	char c;
